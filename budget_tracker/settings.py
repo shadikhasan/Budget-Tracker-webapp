8edfa365393f440c5bc12db9359b116a87bb25b7
SHADIK <shadik.sk420@gmail.com>
2025-01-22 01:44:42 +0600

    gunicorn added

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6z34(se!^^)307t*uog3w+r6i5+!p#!%j@x%+pvjc@pc=#z$-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
